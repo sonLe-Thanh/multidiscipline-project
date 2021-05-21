96fb1960c0d2042e9f9d73cb6d73ff5df5594d9c
Le Thanh Son <thanhson@MacBook-Pro-cua-Son.local>
2021-05-21 14:52:24 +0700

    Complete connect to MQTT server, send and receive data from and to feed

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # multidiscipline-project
-Multidiscipline Project 
+
+Multidiscipline Project
 
 # Requirements
+
 Install expo
 
 Refer to https://docs.expo.io for full information
+
+# Completed tasks
+
+1. Default screen
+2. Register screen
+3. Connect to server
+4. Receive data from feed (io.adafruit.com)
+5. Send data to feed (io.adafruit.com)
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BackGroundNormal from '../components/BackGroundNormal';
 import Header from '../components/Header';
 import Button from '../components/Button';
-
+import {Text} from 'react-native-paper';
+import TextInput from '../components/TextInput';
 
 export default function HomeScreen({navigation}){
+    const userName = "LeThanh";
+    const activeKey = "aio_PoLs042IqMmgNO6SSKa9BaXA90vm";
+    const apiHeader = "https://io.adafruit.com/api/v2/";
+
+    var connectText = 'Connect to Adafruit server';
+    //const [uri, setUri] = useState({value:'', error: ''});
+    const [receiveTopic, setReceiveTopic] = useState({value: '', error:''});
+    const [sendTopic, setSendTopic] = useState({value: ''});
+    const [sendData, setSendData] = useState({value: ''});
+    
+    const connectToAdafruit = (username, key) =>{
+        url = apiHeader+username+"/feeds?x-aio-key="+key;
+        //console.log(url);
+        return fetch(url, {
+            //method: 'POST'
+        }).then((response) => response.json())
+        .then((json) => {
+          console.log("Info: ", json);
+          connectText = 'Conneted to Adafruit server';
+        })
+        .catch((error) => {
+          console.error(error);
+          connectText = 'Error Conneting to Adafruit server';
+        });
+    }
+
+    const getDataFromFeed = (topic, aioKey, mode) =>{
+        url = apiHeader+topic.value+"/data/"+mode;
+        //console.log(url);
+        return fetch(url, {
+            method: "GET",
+            headers: {
+                "X-AIO-Key": aioKey,
+            }
+        }).then((response)=> response.json())
+        .then((json)=>{
+            console.log("Lasted info on topic "+topic.value+":",json);
+            console.log(json.value);
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+    }
+
+    const sendDataToFeed = (topic, aioKey, sendData) =>{
+        url = apiHeader+topic.value+"/data";
+        console.log(url);
+        return fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+                "X-AIO-Key": aioKey,
+            },
+            body: JSON.stringify({
+                value: sendData.value,
+            })
+        }).then((response)=>response.json())
+        .then((json)=>{
+            console.log("Sent data to topic "+topic.value+" with value: "+sendData);
+            console.log("Result: ",json)
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+    }
+
     return (
         <BackGroundNormal>
             <Header>HOME SCREEN HERE</Header>
+
+            <Button mode="contained"
+                onPress={() => connectToAdafruit(userName, activeKey)}            
+            >
+                {connectText}
+            </Button>
+
+            <TextInput
+                label="ReceiveTopicName"
+                returnKeyType="done"
+                value={receiveTopic}
+                onChangeText = {(text)=> setReceiveTopic({value: text, error: ""})}
+                error={!!receiveTopic.error}
+                errorText={receiveTopic.error}
+            />
+
+            <Button
+                mode="contained"
+                onPress={()=>getDataFromFeed(receiveTopic, activeKey, "last")}
+            >
+                Get latest data from topic
+            </Button>
+
+            <TextInput
+                label="SendTopicName"
+                returnKeyType="done"
+                value={sendTopic}
+                onChangeText = {(text)=> setSendTopic({value:text})}
+            />
+
+            <TextInput
+                label="SendData"
+                returnKeyType="done"
+                value={sendData}
+                onChangeText = {(text)=> setSendData({value:text})}
+            />
+
+            <Button
+                mode="contained"
+                onPress={()=>sendDataToFeed(sendTopic, activeKey, sendData)}
+            >
+                Send data to inputed feed
+            </Button>
+
             <Button mode="contained" 
                 onPress={()=>{navigation.reset({
                     index: 0,
